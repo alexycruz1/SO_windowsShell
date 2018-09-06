c2fa6a9831a7f0132534a68930687c03d028e50f
Alexy Cruz <alexyc@unitec.edu>
2018-09-05 22:43:51 -0600

    Primera parte completada

---
+++ miniShell.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <cstddef>
+#include <string.h>
+
+#define N 30
+using namespace std;
+
+int main()
+{
+    char *comando1, *comando2;
+    char *opcion1, *opcion2;
+    char linea[N];
+
+    char *argument[N];
+
+    pid_t pid;
+
+    int wstatus;
+    int errorNo;
+
+    bool tuberia = true;
+
+    while (true)
+    {
+        comando2 = NULL;
+        errorNo = 0;
+        cout << "$: ";
+        cin.getline(linea, N);
+
+        for (int i = 0; linea[i] != '\0'; i++)
+        {
+            if (linea[i] == '|')
+            {
+                comando1 = strtok(linea, "|");
+                comando2 = strtok(NULL, "|");
+
+                tuberia = false;
+
+                if (pid = fork() == 0)
+                {
+                    int pid;
+                    int pipeStatus[2];
+
+                    pipe(pipeStatus);
+
+                    if ((pid = fork()) < 0)
+                    {
+                        cerr << "Fork error al ejecutar el comando pipe" << endl;
+                        exit(1);
+                    }
+
+                    if (pid == 0)
+                    {
+                        close(1);
+                        dup(pipeStatus[1]);
+
+                        close(pipeStatus[0]);
+                        close(pipeStatus[1]);
+
+                        execvp(comando1, NULL);
+                    }
+                    else
+                    {
+                        close(0);
+                        dup(pipeStatus[0]);
+
+                        close(pipeStatus[0]);
+                        close(pipeStatus[1]);
+
+                        execvp(comando2, NULL);
+                    }
+                }
+            }
+        }
+
+        comando1 = strtok(linea, " ");
+        opcion1 = strtok(NULL, " ");
+
+        if ((pid = fork()) == 0 && comando2 == NULL)
+        {
+            errorNo = execlp(comando1, comando1, opcion1, NULL);
+        }
+
+        if (errorNo == -1)
+        {
+            cout << "Comando no encontrado" << endl;
+        }
+        else
+        {
+            wait(&wstatus);
+        }
+    }
+}
